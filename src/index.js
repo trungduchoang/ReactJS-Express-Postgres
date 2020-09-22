bd30826c0b470341e5dc15934df4f2905444b92e
t-hoang-cna <t-hoang-cna@usen-next.jp>
2020-09-22 11:23:00 +0700

    Working Express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import "dotenv/config";
-import sayHello from "./sayHello";
+import "dotenv/config"; // Make env vars available // Deploy need its all .env
+import express from "express";
+import cors from "cors";
 
-sayHello();
+const app = express();
+
+app.use(cors()); // Lib for Remove CORS
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
